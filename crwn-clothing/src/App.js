2b3a916067b2c165626de19dddd2706d42cfe5fa
Barborka Kubalcova <barborka.kubalcova@gmail.com>
2022-02-07 16:06:56 -0100

    added shop page, collection preview and item

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  )
-  };
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
-      
-     
-        {/*  <HomePage/>  */}
+      <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact  path="/hats" component={HatsPage} />
-  
-      
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
